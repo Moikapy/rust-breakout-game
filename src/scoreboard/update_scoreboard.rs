6c8ed08298ee1ebfb20bc439cf10fc02f788196f
Warren <moikapy@moikaslookout.com>
2023-10-22 16:00:19 -0400

    created a module for scoreboard

--- src/scoreboard/update_scoreboard.rs
+++ src/scoreboard/update_scoreboard.rs
@@ -1,5 +1,5 @@
-mod scoreboard;
-use scoreboard::Scoreboard_Structs as Scoreboard;
+use crate::scoreboard::Scoreboard;
+use bevy::prelude::*;
 pub fn update_scoreboard(score: Res<Scoreboard>, mut query: Query<&mut Text>) {
         let mut text = query.single_mut();
         text.sections[1].value = score.score.to_string();
